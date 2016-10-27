a3851e64e2df2ca033793340213a390dbfd020ae
Hieu Nguyen <hnnguyen45@gmail.com>
2016-10-26 23:54:32 -0700

    Still working on HTML, CSS and JS

--- js/app.js
+++ js/app.js
@@ -22,3 +22,17 @@ var saltyIngredients = ["olive on a stick", "salt-dusted rim", "rasher of bacon"
 var bitterIngredients = ["shake of bitters", "splash of tonic", "twist of lemon peel"];
 var sweetIngredients = ["sugar cube", "spoonful of honey", "splash of cola"];
 var fruityIngredients = ["slice of orange", "dash of cassis", "cherry on top"];
+
+//Creating the cocktail names object for each one with random adjectives nouns
+//Here is the adjectives for the following drinks http://adjective1.com/for-drinks/
+//Here is the nouns for the following drinks http://adjective1.com/for-drinks/
+var cocktailNames = {
+	adjectives: ["Fuzzy", "Salty", "Illegal", "Infamous", "Dirty", "Sea", "Ruthless"],
+	nouns: ["Vessel", "First-Mate", "Eye", "Ship", "Maggot", "Float", "Parrot"],
+	getRandomAdjective: function(){
+		return cocktailNames.adjectives[Math.floor(Math.random()*cocktailNames.adjectives.length)]
+	},
+	getRandomNoun: function(){
+		return cocktailNames.nouns[Math.floor(Math.random()*cocktailNames.nouns.length)]
+	},
+}
