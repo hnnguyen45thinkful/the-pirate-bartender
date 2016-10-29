535e621c7568d9c36f19785266ececc498fcb950
Hieu Nguyen <hnnguyen45@gmail.com>
2016-10-29 00:19:53 -0700

    Tested and completed JS and CSS still modifying and checking.

--- js/app.js
+++ js/app.js
@@ -1,3 +1,4 @@
+//Created and modified by Hieu Nguyen.
 //Define the bartender function which refer to the specific object instance which you are working with pirate.
 var Bartender= function(pirate){
 	this.pirate = pirate
@@ -44,7 +45,7 @@ var pirateBartender= Object.create(Bartender.prototype)
 
 //Now I need to create bartender functions which shows all the methods shared and shows the ingredients from the user's preferences randomly. From there, creates a drink with a cool nickname.
 Bartender.prototype.createDrink= function(state){
-	if (state.userPreferences.length===0) return ["No ingredients for you, I am SOBER!!!"]
+	if (state.userPreferences.length === 0) return ["No ingredients for you, I am SOBER!!!"]
 	var drink = state.userPreferences.map(function(index){
 		var ingredientCategory = pantryIngredients[index] // ["slice of orange", "dash of cassis", "cherry on top"]
 		return ingredientCategory[Math.floor(Math.random()*ingredientCategory.length)]
@@ -59,3 +60,83 @@ var cocktailNameCreator = function(){
 var currentQuestion = function(state){
 	return arrayQuestions[state.currentQuestionIndex];
 }
+//Applying the DOM to help the content display with jquery. To show the questions from the array from the top pushing it. 
+var renderCurrentQuestion = function (){
+	var question = currentQuestion(state)
+	var questionHTML= "<h2>"+question+"</h2>"
+	$(".question").html(questionHTML)
+}
+
+//Creating a function to help render the drink created by the bartender in the DOM in using the ingredients items.
+var renderDrink = function(pirateDrink){
+	var drinkHTML = pirateDrink.map(function(item){
+		return "<li>"+item+"</li>"
+	})
+	$(".drinkIngredients").html(drinkHTML)
+}
+
+//By creating the function to render the drink's name by showing a random adjectives and nouns together. 
+var renderDrinkName= function(cocktailRandomName){
+	var drinkNameHTML= "<h2>The "+cocktailRandomName+"</h2>"
+	$(".drinkName").html(drinkNameHTML)
+}
+
+//Also, creating an error function to help render the error.
+var renderError= function(element){
+	var errorHTML= "<h2>So ye got no scurvy pirate taste! Goodbye!</h2>"
+	element.html(errorHTML)
+}
+
+//Applying the jQuery functions to modify the DOM for displaying the results and questions respectfully. 
+var showResults= function(){
+	$(".questionContainer").hide()
+	$(".drinkContainer").show()
+}
+var showQuestions= function(){
+	$(".questionContainer").show()
+	$(".drinkContainer").hide()
+}
+
+// Event listner for yes button. Calls functions and methods to create a drink and renders content into the DOM
+$(".yesBtn").on("click", function(event){
+	event.preventDefault();
+	// Guardar los indexes de las categorias que el usuario eligio en un array.
+	state.userPreferences.push(state.currentQuestionIndex);
+	userChosePreference();
+})
+// event listner for no button. Renders the next question
+$(".noBtn").on("click", function(event){
+	event.preventDefault()
+	userChosePreference();
+})
+
+userChosePreference = function(){
+	state.currentQuestionIndex++;
+	if(state.currentQuestionIndex < arrayQuestions.length){
+		renderCurrentQuestion(state)
+	}
+	if(state.currentQuestionIndex === arrayQuestions.length){
+		makeDrink()
+	}
+}
+//Showing all the results in the container together to making the drink.
+var makeDrink = function(){
+	var pirateDrink = pirateBartender.createDrink(state)
+	var cocktailRandomName = cocktailNameCreator()
+	showResults()
+	renderDrink(pirateDrink)
+	renderDrinkName(cocktailRandomName)
+}
+
+// event listner for starting again/creating a new drink
+$(".drinkContainer").on("click","button", function(){
+	showQuestions()
+	state.currentQuestionIndex = 0;
+	state.userPreferences = [];
+	renderCurrentQuestion()
+})
+
+// Creating the first question to load on the webpage in the DOM to start and navagate. 
+$(document).ready(function(){
+	renderCurrentQuestion()
+})
